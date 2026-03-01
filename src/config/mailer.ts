e0b783104f4f9be8f28ba80c21c101377e38863c
Jolyeh <yehezekocolojoelprince@gmail.com>
2026-03-01 16:21:29 +0100

    fix env 2

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -4,7 +4,7 @@ const user = process.env.SMTP_USER;
 const pass = process.env.SMTP_PASS;
 
 if (!user || !pass) {
-  throw new Error("EMAIL_USER ou EMAIL_PASS manquant dans .env");
+  throw new Error("SMTP_USER ou SMTP_PASS manquant dans .env");
 }
 
 export const transporter = nodemailer.createTransport({
